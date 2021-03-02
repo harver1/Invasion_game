7d6285037a757664eb135e365789483854af8312
harver1 <harver.one@gmail.com>
2021-03-03 00:48:06 +0300

    add controll

--- Invasion.py
+++ Invasion.py
@@ -12,16 +12,17 @@ class Invasion:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect(). height
         pygame.display.set_caption("Invasion")
         self.ship = Ship(self)
-
-        # Screen color
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Starting the main game cycle."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -29,6 +30,26 @@ class Invasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """react to key pressing."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """react to letting go key."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Updating image on the screen and flip to the new screen"""
--- ship.py
+++ ship.py
@@ -8,14 +8,27 @@ class Ship:
         """Ship initializig and start position."""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-
         # Load ship image
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
-        # All nes ships start from lower
+        # All new ships start from lower
         self.rect.midbottom = self.screen_rect.midbottom
+        # Save ship coords
+        self.x = float(self.rect.x)
+        # flag for moving
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update x position"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # update rect
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw ship in current position."""
