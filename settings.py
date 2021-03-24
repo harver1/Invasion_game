c39d9bd4adce1a383c57c53582e1f1c8c1444f74
harver1 <harver.one@gmail.com>
2021-03-24 17:21:35 +0300

    123

--- settings.py
+++ settings.py
@@ -9,3 +9,9 @@ class Settings:
         self.bg_color = (230, 230, 230)
         # Ship settings
         self.ship_speed = 1.5
+        # bullet params
+        self.bullet_speed = 1
+        self.bullet_with = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
